d4d70f1b6d01d4099e0631c80ac3bf04e8d3e835
Sushil Maurya <sushilm2011@gmail.com>
2019-01-05 03:09:16 +0530

    Demo to add text from EditText

--- app/src/main/java/com/sushilmaurya/firebasetutorial/MainActivity.java
+++ app/src/main/java/com/sushilmaurya/firebasetutorial/MainActivity.java
@@ -2,22 +2,29 @@ package com.sushilmaurya.firebasetutorial;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
-        DatabaseReference databaseReference = firebaseDatabase.getReference().child("MyMessages");
-        databaseReference.removeValue();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference().child("MyMessages");
 
-        //TODO: 4.Create an Edit Text (Name) and a Button Saying Save and Try Saving that data to the database
 
+    }
+
+    public void btnSaveClicked(View view) {
+        EditText etMessage = findViewById(R.id.etName);
+        databaseReference.setValue(etMessage.getText().toString());
     }
 }
